e4ed8834b03bc97e7f9f58ea0d64a578bae97c5f
Toan211203 <toancoi123452022@gmail.com>
2024-04-16 09:41:25 +0700

    fixing form border

--- N_Puzzle_Game/View/Eight_Puzzle.Designer.cs
+++ N_Puzzle_Game/View/Eight_Puzzle.Designer.cs
@@ -100,7 +100,7 @@
 			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.panel1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Margin = new System.Windows.Forms.Padding(4);
 			this.MaximizeBox = false;
